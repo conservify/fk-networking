e2e2658e457732c4286037a2341982b305406cca
Jacob Lewallen <jlewallen@gmail.com>
2020-11-12 16:16:57 -0800

    File copy method.

--- app/src/main/java/org/conservify/data/FileSystem.java
+++ app/src/main/java/org/conservify/data/FileSystem.java
@@ -1,10 +1,18 @@
 package org.conservify.data;
 
 import android.content.Context;
+import android.util.Log;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class FileSystem {
+    private static final String TAG = "JS";
+
     private final AtomicLong tokens = new AtomicLong();
     private final Context context;
     private final FileSystemListener listener;
@@ -25,5 +33,30 @@ public class FileSystem {
 
     public String newToken() {
         return "cfyfs-" + tokens.incrementAndGet();
+    }
+
+    public boolean copyFile(String source, String destiny) {
+        try {
+            InputStream is = new FileInputStream(source);
+            try {
+                OutputStream os = new FileOutputStream(destiny);
+                try {
+                    byte[] buf = new byte[32768];
+                    int len;
+                    while ((len = is.read(buf)) > 0) {
+                        os.write(buf, 0, len);
+                    }
+                } finally {
+                    os.close();
+                }
+            } finally {
+                is.close();
+            }
+            return true;
+        }
+        catch (IOException e) {
+            Log.e(TAG, "error", e);
+            return false;
+        }
     }
 }
