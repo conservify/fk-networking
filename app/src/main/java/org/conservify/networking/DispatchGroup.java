741ab7aa40ffee15876b1bc28c2723e29dff2726
Jacob Lewallen <jlewallen@gmail.com>
2021-02-16 09:11:56 -0800

    Move entire start/stop process into background thread.

--- app/src/main/java/org/conservify/networking/DispatchGroup.java
+++ app/src/main/java/org/conservify/networking/DispatchGroup.java
@@ -17,41 +17,62 @@ public class DispatchGroup {
     public synchronized void leave() {
         long value = counter.decrementAndGet();
         if (value == 0) {
+            Log.i(TAG, "DispatchGroup.leave - notifyGroup!");
             notifyGroup();
         } else if (value < 0) {
+            Log.e(TAG, "DispatchGroup.leave - negative!");
             throw new RuntimeException(String.format("DispatchGroup: excess leave calls: {}", value));
         }
     }
 
     public void notify(Runnable r) {
-        Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ")");
-        runnable.set(r);
-        notifyGroup();
-        Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ") done");
+        try
+        {
+            Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ") start");
+            if (runnable == null) {
+                Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ") start: " + "NULL");
+            }
+            else {
+                Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ") start: " + runnable.get());
+            }
+            Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ") start: " + r);
+            runnable.set(r);
+            Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ") notifying");
+            notifyGroup();
+            Log.i(TAG, "DispatchGroup::notify(" + counter.get() + ") done");
+        }
+        catch (Exception e) {
+            Log.e(TAG, "DispatchGroup.notify failed:", e);
+        }
     }
 
     private synchronized void notifyGroup() {
-        long value = counter.get();
-        Log.i(TAG, "DispatchGroup::notifyGroupInside(" + value + ")");
-        if (value == 0) {
-            Runnable running = runnable.getAndSet(null);
-            if (running != null) {
-                Log.i(TAG, "DispatchGroup::invoking");
-                try {
-                    running.run();
+        try {
+            long value = counter.get();
+            Log.i(TAG, "DispatchGroup::notifyGroupInside(" + value + ")");
+            if (value == 0) {
+                Runnable running = runnable.getAndSet(null);
+                if (running != null) {
+                    Log.i(TAG, "DispatchGroup::invoking");
+                    try {
+                        running.run();
+                    }
+                    catch (Exception e) {
+                        Log.e(TAG, "DispatchGroup.notifyGroup(run) failed:", e);
+                    }
+                    finally {
+                        Log.i(TAG, "DispatchGroup::invoked");
+                    }
                 }
-                catch (Exception e) {
-                    Log.e(TAG, "DispatchGroup.notify failed:", e);
+                else {
+                    Log.i(TAG, "DispatchGroup::late notifyGroup()");
                 }
-                finally {
-                    Log.i(TAG, "DispatchGroup::invoked");
-                }
+            } else if (value < 0) {
+                Log.i(TAG, "DispatchGroup::negative notifyGroup()");
             }
-            else {
-                Log.i(TAG, "DispatchGroup::late notifyGroup()");
-            }
-        } else if (value < 0) {
-            Log.i(TAG, "DispatchGroup::negative notifyGroup()");
+        }
+        catch (Exception e) {
+            Log.e(TAG, "DispatchGroup.notifyGroup failed:", e);
         }
     }
 
